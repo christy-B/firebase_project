c023a262301f1434cb1136e7b543aa1fffeb91c6
Ebou <bienechristy94@gmail.com>
2023-12-22 15:17:23 +0100

    devcontainer

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div style={{minHeight:'100vh',display:'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <button style={{padding:'10px 20px', backgroundColor:'#007bff', color:'white', border:'none', borderRadius:'5px', cursor:'pointer' }}>FIREBASE PROJECT</button>
+    </div>
+  
+  );
+}
+
+export default App;
