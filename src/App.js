dc2abd9f1dcfd2d6f797e747904b1819fdca92f1
Christelle Nebie Ebou <christelle.nebieebou@wynd.eu>
2024-01-16 11:33:10 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignIn from './Component/Auth';
 
 function App() {
   return (
     <div style={{minHeight:'100vh',display:'flex', justifyContent: 'center', alignItems: 'center'}}>
-      <button style={{padding:'10px 20px', backgroundColor:'#007bff', color:'white', border:'none', borderRadius:'5px', cursor:'pointer' }}>FIREBASE PROJECT</button>
+      <div>
+        <h1 style={{color:'#007bff' }}>FIREBASE PROJECT</h1>
+        <SignIn/>
+      </div>
     </div>
   
   );
